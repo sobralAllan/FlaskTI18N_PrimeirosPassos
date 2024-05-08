d51fb13f4666e341d15a1115d4f10dc046319f08
ALLAN SOBRAL DA SILVA <allan.ssilva4@senacsp.edu.br>
2024-05-07 21:44:48 -0300

    primeiro commit

---
+++ app/__init__.py
@@ -0,0 +1,3 @@
+from flask import Flask
+app = Flask(__name__) #Criando a variável app
+from app import routes
---
+++ app/routes.py
@@ -0,0 +1,14 @@
+from app import app
+from flask import render_template
+from flask import request
+
+@app.route('/') #Configurando uma rota web
+@app.route('/index')
+def index():
+    return render_template('index.html', titulo="Página Inicial", nome="Allan")
+
+@app.route('/contato')
+def contato():
+    return render_template('contato.html', titulo="Contato", nome="Allan")
+
+
